fc40ff2aa80c85eb63e9f2536e9e02ca38efc0cc
ARashadev <ahm.rashad.dev@gmail.com>
2023-02-18 18:41:08 +0200

    Updates

---
+++ precache-manifest.dbbb1aaad09e839a4902ebc5604acc95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ec521bd3fe165486fc4",
+    "url": "/ztm-reactjs-robofriends/static/css/main.4a4d53d6.chunk.css"
+  },
+  {
+    "revision": "0ec521bd3fe165486fc4",
+    "url": "/ztm-reactjs-robofriends/static/js/main.0ec521bd.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/ztm-reactjs-robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "8841d973b11b31f8845b",
+    "url": "/ztm-reactjs-robofriends/static/css/2.ca2fe507.chunk.css"
+  },
+  {
+    "revision": "8841d973b11b31f8845b",
+    "url": "/ztm-reactjs-robofriends/static/js/2.8841d973.chunk.js"
+  },
+  {
+    "revision": "1ff42af27e27cba572de",
+    "url": "/ztm-reactjs-robofriends/static/js/runtime~main.1ff42af2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ztm-reactjs-robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "30afddbba63d4e555963773d7a312f25",
+    "url": "/ztm-reactjs-robofriends/index.html"
+  }
+];
